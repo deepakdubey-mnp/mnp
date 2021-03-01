e8a2bcd184d0d829c9773358936bd648a0f4e3ef
Deepak Dubey <deepak_dubey@epam.com>
2021-03-01 13:05:54 +0530

    Added initial

---
+++ java-spring/rabbit/src/main/java/io/ls/messaging/rabbiting/RabbitingApplication.java
@@ -0,0 +1,13 @@
+package io.ls.messaging.rabbiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitingApplication.class, args);
+    }
+
+}
